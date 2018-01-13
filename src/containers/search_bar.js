12f7a9c3db987da88234138a8d2dd88fc9987e24
AlexTicovschi <alexmticovschi@gmail.com>
2018-01-13 10:28:13 +0000

    controlled components a binding context

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -1,10 +1,32 @@
 import React, { Component } from 'react';
 
 export default class SearchBar extends Component {
+	constructor(props) {
+		super(props);
+
+		// the value of the input is going to be mapped to this.state.term
+		this.state = { term: '' };
+
+		// this instance of SearchBar has a function called onInputChange
+		// bind onInputChange function to SearchBar and replace onInputChange with 
+		// a new bound instance of this function
+		this.onInputChange = this.onInputChange.bind(this);
+	}
+
+	onInputChange(event) {
+		console.log(event.target.value);
+		this.setState({ term: event.target.value });
+	}
+
   render() {
     return (
       <form className="input-group">
-        <input className="from-control" />
+				<input 
+					className="form-control" 
+					placeholder="Get a five-day forecast in your favourite cities"
+					value={this.state.term}
+					onChange={this.onInputChange}
+				/>
         <span className="input-group-btn">
           <button type="submit" className="btn btn-secondary">Submit</button>
         </span>
