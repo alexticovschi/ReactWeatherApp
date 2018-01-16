690060794b733aef3f3a04e64dff7fe0837b3fb8
AlexTicovschi <alexmticovschi@gmail.com>
2018-01-16 09:52:51 +0000

    avoiding state mutations in reducers

--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -1,6 +1,14 @@
+import { FETCH_WEATHER } from '../actions/index';
+
 // a reducer that takes an action
-// the default state is set to null
-export default function(state = null, action) {
-  console.log('Action received', action);
+// the default state is an array
+export default function(state = [], action) {
+  switch (action.type) {
+    case FETCH_WEATHER:
+      // make a new array, put action.data inside of it and then take all the inside entries 
+      // of the variable state, and insert it into new outside array
+      return [ action.payload.data, ...state ];
+  }
+
   return state;
 }
