9820605b1bb89a4c0fda835e5fa6b89def8b4b53
AlexTicovschi <alexmticovschi@gmail.com>
2018-01-17 19:45:40 +0000

    fixing bug searching for nonexistent city

--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -3,6 +3,11 @@ import { FETCH_WEATHER } from '../actions/index';
 // a reducer that takes an action
 // the default state is an array
 export default function(state = [], action) {
+  // if the city is nonexistent, return the state
+  if(action.error) {
+    return state;
+  }
+
   switch (action.type) {
     case FETCH_WEATHER:
       // make a new array, put action.data inside of it and then take all the inside entries 
